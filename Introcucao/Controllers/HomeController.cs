a7e03cd3b59d75912b2892b7e2a9a2cd39e59d1b
fmoc65 <fmoc65@gmail.com>
2020-05-30 19:30:27 -0300

    Add project files.

---
+++ Introcucao/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Introcucao.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return  View();
+        }
+
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Introcucao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Introcucao.MvcApplication" Language="C#" %>
