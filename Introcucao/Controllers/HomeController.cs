57ff75612efe03186e4f5eb2b669ba0c812a0491
fmoc65 <fmoc65@gmail.com>
2020-05-31 09:58:32 -0300

    ++

--- Introcucao/Controllers/HomeController.cs
+++ Introcucao/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 
+
 namespace Introcucao.Controllers
 {
     public class HomeController : Controller
@@ -7,7 +8,17 @@ namespace Introcucao.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return  View();
+            var pessoa = new Models.Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Jose Carlos",
+                Tipo = "Administrador"
+            };
+
+            //ViewBag.Id = pessoa.PessoaId;
+            //ViewBag.Nome = pessoa.Nome;
+            //ViewBag.Tipo = pessoa.Tipo;
+            return  View(pessoa);
         }
 
         public ActionResult Contatos()
